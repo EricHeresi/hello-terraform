abdc1d37c2c27671e8da8ab679e89fc958508fe5
ericheresi <eheresimedina@gmail.com>
2023-02-15 13:11:15 +0100

    ansible integration with terraform, installing apache

--- main.tf
+++ main.tf
@@ -25,36 +25,7 @@ resource "aws_instance" "app_server" {
     Name = var.instance_name
     APP  = "vue2048"
   }
-  provisioner "file" {
-    source      = "userdata.sh"
-    destination = "/tmp/userdata.sh"
-    connection {
-      type = "ssh"
-      user = "ec2-user"
-      host = self.public_ip
-    }
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/userdata.sh",
-      "sudo /tmp/userdata.sh",
-    ]
-    connection {
-      type = "ssh"
-      user = "ec2-user"
-      host = self.public_ip
-    }
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "cd hello2048",
-      "docker-compose pull",
-      "docker-compose up -d",
-    ]
-    connection {
-      type = "ssh"
-      user = "ec2-user"
-      host = self.public_ip
-    }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${self.public_ip}, ec2-config.yml"
   }
 }
