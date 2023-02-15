7302017b68de475ca0c8d5513f37009e60ecaf34
ericheresi <eheresimedina@gmail.com>
2023-02-15 12:18:36 +0100

    switched to use provisioners, not recommended

--- main.tf
+++ main.tf
@@ -25,6 +25,36 @@ resource "aws_instance" "app_server" {
     Name = var.instance_name
     APP  = "vue2048"
   }
-  user_data                   = file("${path.module}/userdata.sh")
-  user_data_replace_on_change = true
+  provisioner "file" {
+    source      = "userdata.sh"
+    destination = "/tmp/userdata.sh"
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      host = self.public_ip
+    }
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/userdata.sh",
+      "sudo /tmp/userdata.sh",
+    ]
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      host = self.public_ip
+    }
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "cd hello2048",
+      "docker-compose pull",
+      "docker-compose up -d",
+    ]
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      host = self.public_ip
+    }
+  }
 }
