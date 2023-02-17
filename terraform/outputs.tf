0d181a0368f5e9b1d7b0d618dd6863003f4de378
ericheresi <eheresimedina@gmail.com>
2023-02-17 09:19:54 +0100

    fixed typo bug

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "ec2_map" {
   description = "ID of the EC2 Instances"
-  value       = { for i in aws_instance.ec2instance : i.id => "${i.id}:${i.public_ip}" }
+  value       = { for i in aws_instance.app_server : i.id => "${i.id}:${i.public_ip}" }
 }
